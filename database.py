0ada0e87f69aa872dea0bcfe8c483fe9d8f75e95
FelipeGehren <felipegehren@gmail.com>
2025-06-16 19:59:19 -0300

    database

--- database.py
+++ database.py
@@ -1,7 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import pymysql
 
+# Primeiro, conectamos sem especificar o banco de dados
+connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='root'
+)
+
+try:
+    with connection.cursor() as cursor:
+        # Criar o banco de dados se não existir
+        cursor.execute("CREATE DATABASE IF NOT EXISTS meu_projeto")
+    connection.commit()
+finally:
+    connection.close()
+
+# Agora conectamos ao banco de dados criado
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/meu_projeto"
 
 engine = create_engine(
